b658a90ba5f84dad12aa4494ad9a07cdf31403f7
levanthoi <lethoi1506@gmail.com>
2024-02-07 22:50:44 +0700

    project structue

--- app/src/main/java/com/example/book_pitch/Activities/MainActivity.java
+++ app/src/main/java/com/example/book_pitch/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.book_pitch;
+package com.example.book_pitch.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.book_pitch.R;
 
 public class MainActivity extends AppCompatActivity {
 
